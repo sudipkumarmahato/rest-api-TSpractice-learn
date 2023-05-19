8a55f28bc8b132290978f9303702c30c1ce0eccb
Sudip Kumar Mahato <1234sudipmahato@gmail.com>
2023-05-19 22:51:15 +0545

    added-prettier

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import compression from "compression";
-import http from "http";
-import mongoose from "mongoose";
-import morgan from "morgan";
-import dotenv from "dotenv";
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import http from 'http';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 
 app.use(
-  cors({
-    credentials: true,
-  })
+    cors({
+        credentials: true
+    })
 );
 app.use(compression());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(bodyParser.json());
 
@@ -27,17 +27,17 @@ const port = process.env.SERVER_PORT || process.env.DEFAULT_PORT;
 const db = process.env.MONGOURI;
 
 mongoose
-  .connect(db)
-  .then(() => {
-    server.listen(port, () => {
-      console.log(
-        `DB Connected and Server listening on port ${port} Successfully`
-      );
+    .connect(db)
+    .then(() => {
+        server.listen(port, () => {
+            console.log(
+                `DB Connected and Server listening on port ${port} Successfully`
+            );
+        });
+    })
+    .catch((err) => {
+        console.log(err);
     });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
 
 // server.listen(port, () => {
 //   console.log(`Server listening on port ${port}`);
