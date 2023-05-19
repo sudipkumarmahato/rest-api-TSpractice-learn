23bc1df6d112b3892df2ebca303bc6fa0341defb
Sudip Kumar Mahato <1234sudipmahato@gmail.com>
2023-05-19 23:29:52 +0545

    added users.model

---
+++ src/models/users.ts
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    authentication: {
+        password: {
+            type: String,
+            required: true,
+            select: false
+        },
+        salt: {
+            type: String,
+            required: true,
+            select: false
+        },
+        sessionToken: {
+            type: String,
+            required: true,
+            select: false
+        }
+    }
+});
+
+export const User = mongoose.model('User', userSchema);
