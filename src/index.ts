defb94b2a336cf3301a9dbc51883b125ce84802c
Sudip Kumar Mahato <1234sudipmahato@gmail.com>
2023-05-19 16:20:06 +0545

    ts project setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,42 @@
-console.log("Hello  world");
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import http from "http";
+import mongoose from "mongoose";
+import morgan from "morgan";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+app.use(compression());
+app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+const port = process.env.SERVER_PORT || process.env.DEFAULT_PORT;
+const db = process.env.MONGOURI;
+
+mongoose
+  .connect(db)
+  .then(() => {
+    server.listen(port, () => {
+      console.log(`Server listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// server.listen(port, () => {
+//   console.log(`Server listening on port ${port}`);
+// });
