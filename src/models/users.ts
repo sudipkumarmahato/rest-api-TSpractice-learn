54e1130581050668bbf7364c5a232bfa24b4e76a
Sudip Kumar Mahato <1234sudipmahato@gmail.com>
2023-05-24 08:45:34 +0545

    usermodel-updated

--- src/models/users.ts
+++ src/models/users.ts
@@ -28,4 +28,18 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-export const User = mongoose.model('User', userSchema);
+export const UserModel = mongoose.model('User', userSchema);
+
+export const getUser = () => UserModel.find();
+export const getUserByEmail = (email: string) => UserModel.findOne({ email });
+export const getUserBySesssionToken = (sessionToken: string) =>
+    UserModel.findOne({ 'authentication.sessionToken': sessionToken });
+export const getUserByID = (id: string) => UserModel.findById(id);
+
+export const createUser = () => (values: Record<string, any>) =>
+    new UserModel(values).save().then((user) => user.toObject());
+export const deleteUserByID = (id: string) =>
+    UserModel.findOneAndDelete({ _id: id });
+
+export const updateUserByID = (id: string, values: Record<string, any>) =>
+    UserModel.findByIdAndUpdate({ id, values });
